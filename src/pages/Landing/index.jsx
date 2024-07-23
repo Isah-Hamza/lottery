4c7e723de067d40621c6fc812eb86e48f3d45887
Isah Hamza <itshamzy@gmail.com>
2024-07-23 11:37:57 +0100

    -

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -56,45 +56,45 @@ const index = () => {
   return (
     <>
         <div className='flex bg-[#f5f7f8]'>
-        <div className="w-[500px] 2xl:w-[600px] h-screen bg-primary flex flex-col p-5 ">
+        <div className="hidden lg:flex w-[500px] 2xl:w-[600px] h-screen bg-primary flex-col p-5 ">
             <img className='w-12' src={logo} alt="logo" />
             <div className="flex-1 bg-red-200n grid place-content-center">
                 <img className='w-full' src={design} alt="design" />
             </div>
         </div>
-        <div className="flex-1 h-screen overflow-auto p-10 py-7 pb-10">
+        <div className="flex-1 h-screen overflow-auto p-5 sm:p-10 py-7 pb-10">
             <div className="flex justify-end items-center gap-2 text-sm">
                 {/* <span className='text-faint_black'>Already have an account ?</span> */}
                 <button onClick={goBack} className='font-medium bg-white text-black px-5 py-3 shadow rounded-sm' >GO BACK</button>
             </div>
-        {step == 1 ?  <div className="mt-20 mx-auto w-[720px] bg-white shadow min-h-[200px] rounded-md p-6 pt-7  pb-8">
+        {step == 1 ?  <div className="mt-20 mx-auto w-full lg:w-[720px] bg-white shadow min-h-[200px] rounded-md p-6 pt-7  pb-8">
                 <div className="text-center text-sm">
                     <p className='text-lg font-semibold text-primary'>Lorem ipsum dolor sit amet.</p>
                     <p className='text-faint_black' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque.</p>
                 </div>
-                <div className="mt-14 grid grid-cols-2 gap-6">
+                <div className="mt-14 grid sm:grid-cols-2 gap-6">
                     <div className=""><Select label={'Select Operator'} /></div>
                     <div className=""><Select label={'Select Service'} /></div>
                     <div className=""><Select label={'Billing Currency'} /></div>
                     <div className=""><Input label={'Amount'} placeholder={'₦ 120,000'}/></div>
                     <div className=""><Select label={'Country of Operation'} /></div>
                     <div className=""><Select label={'State/County'} /></div>
-                    <div className="col-span-2">
+                    <div className="sm:col-span-2">
                         <p className='text-sm mb-1' >Description</p>
                         <textarea className='placeholder:text-sm p-3 border outline-none w-full h-28 rounded' 
                             placeholder="I'm making payment on behalf ..." />
                     </div>
-                    <div className="text-sm -mt-5 col-span-2 flex gap-2 items-start text-faint_black">
+                    <div className="text-sm -mt-5 sm:col-span-2 flex gap-2 items-start text-faint_black">
                         <input id='check' type='checkbox' className='accent-primary mt-2'/>
                         <label htmlFor='check' className='pt-1' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione adipisci odio magni, incidunt eligendi alias quas illum. Saepe, ea non.</label>
                     </div>
-                    <div className="mt-10 flex justify-center col-span-2">
+                    <div className="mt-10 flex justify-center sm:col-span-2">
                         <button onClick={next} className='uppercase px-5 py-3 rounded text-sm bg-primary text-white font-semibold' >Continue To Pay</button>
                     </div>
                 </div>
             </div>
             : step == 2 ?
-            <div className="mt-20 mx-auto w-[550px] bg-white shadow min-h-[200px] rounded-md p-6 pt-7  pb-8">
+            <div className="mt-20 mx-auto w-full sm:w-[550px] bg-white shadow min-h-[200px] rounded-md p-6 pt-7  pb-8">
                 <div className="text-center text-sm mb-5">
                     <p className='text-lg font-semibold text-opacity-90 mb-1 text-primary'>Please Verify Your Details.</p>
                     <p className='text-faint_black' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque. Lorem ipsum dolor sit amet. </p>
@@ -137,7 +137,7 @@ const index = () => {
         }
         </div>
         </div>
-        { success ? <div className="text-sm text-center fixed inset-0 bg-white/90 grid place-content-center ">
+        { success ? <div className="p-5 text-sm text-center fixed inset-0 bg-white/90 grid place-content-center ">
             <div className="mx-auto">
             <img className='w-20' src={successImg} alt="success" />
             </div>
